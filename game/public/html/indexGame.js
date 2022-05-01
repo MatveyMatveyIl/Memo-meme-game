c7e49b9e2db55ccdc14a5a4e20a22c4bae8e7d5e
MatveyMatveyIl <matvey.ilichev@gmail.com>
2022-05-01 12:51:09 +0500

    end game with alert

--- game/public/html/indexGame.js
+++ game/public/html/indexGame.js
@@ -1,39 +1,63 @@
-let countFlippedCards = 0;
 const cards = document.querySelectorAll('.memory-card');
 document.querySelector('#start-game').addEventListener('click', startGame);
 cards.forEach(card => {
     card.addEventListener('click', flipCard)
 });
 
+const gameState = {
+    countFlippedCards: 0,
+    isStarted: false,
+}
+
+function generateField() {
+    let fs = require('fs');
+    let files = fs.readdir
+
+}
+
+function startGame() {
+    if(!gameState.isStarted) {
+        gameState.countFlippedCards = 0;
+        gameState.isStarted = true;
+        cards.forEach(card => card.classList.add('flipped'));
+        setTimeout(() => cards.forEach(card => card.classList.remove('flipped')), 1500);
+    }
+}
+
 function flipCard() {
+    if(!gameState.isStarted) return;
     if(this.classList.contains('flipped')) return;
-    ++countFlippedCards;
-    if (countFlippedCards <= 2) {
+    ++gameState.countFlippedCards;
+    if (gameState.countFlippedCards <= 2) {
         this.classList.add('flipped');
     }
 
-    if (countFlippedCards === 2) {
+    if (gameState.countFlippedCards === 2) {
         const flippedCards = document.querySelectorAll('.flipped:not(.checked)');
         if (flippedCards[0].children[0].src === flippedCards[1].children[0].src) {
-            console.log(1)
             flippedCards[0].classList.add('checked');
             flippedCards[1].classList.add('checked');
         }
 
         setTimeout(() => flipCardsBack(), 1000);
     }
-
+    setTimeout(() => defineWin(), 1500);
 }
 
 function flipCardsBack() {
-    countFlippedCards = 0;
+    gameState.countFlippedCards = 0;
     const flippedCards = document.querySelectorAll('.memory-card:not(.checked)');
     for (let card of flippedCards) {
         card.classList.remove('flipped');
     }
 }
 
-function startGame() {
-    cards.forEach(card => card.classList.add('flipped'));
-    setTimeout(() =>cards.forEach(card => card.classList.remove('flipped')), 1500);
+function defineWin() {
+    const countFlippedCards = document.querySelectorAll('.memory-card:not(.checked)').length;
+    if(!countFlippedCards) {
+        gameState.countFlippedCards = 0;
+        gameState.isStarted = false;
+        alert('you win!');
+        setTimeout(() => cards.forEach(card => card.classList.remove('flipped')), 1500);
+    }
 }
