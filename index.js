8560a8a47d4f9f461e39561531185bf860bc6506
egregious <krokhin.study@gmail.com>
2022-05-09 19:32:48 +0500

    fix port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 80
 
 app.use(function (request, response) {
     response.sendFile(__dirname + '/game/public/html/gameStart.html');
 });
 
-app.listen(3000);
+app.listen(PORT);
