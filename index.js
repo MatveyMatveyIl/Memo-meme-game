fdbb8268fde13f396acc6c12d08c4a22ba0bd3a2
dshzhkv <dashunich.f@gmail.com>
2022-05-28 23:55:08 +0500

    login

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 
-
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('gameStart')
-})
+    res.render('gameStart');
+});
 
 app.get('/game', (req, res) => {
-    res.render('indexGame')
-})
+    res.render('indexGame');
+});
 
 app.get('/gameover', (req, res) => {
-    res.render('loseGame')
-})
+    res.render('loseGame');
+});
 
 app.get('/win', (req, res) => {
-    res.render('finishGame')
-})
+    res.render('finishGame');
+});
+
 
 app.listen(PORT);
--- public/js/startGame.js
+++ public/js/startGame.js
@@ -1,5 +1,71 @@
-document.querySelector('#gameStartButton').addEventListener('click', startGame);
+const users = [
+    {
+        username: 'admin',
+        password: 'admin',
+        isAdmin: true,
+        score: 0
+    }
+];
 
-function startGame() {
+function setCookie(key, value)
+{
+    document.cookie= key + "=" + value;
+}
+
+function setCookies(username, isAdmin){
+    setCookie("username", username);
+    setCookie("isAdmin", isAdmin);
+}
+
+const loginForm = document.querySelector('#loginForm');
+const loginMessage = document.querySelector('#loginMessage');
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const username = loginForm.username.value;
+
+    if (username !== 'admin'){
+        loginWithUser(username);
+    } else {
+        loginWithAdmin();
+    }
+});
+
+function startGame(username){
+    setCookies(username, false);
     window.location.href = '/game';
+}
+
+function loginWithUser(username) {
+    if (username === ''){
+        loginMessage.style.opacity = '1';
+        loginMessage.innerHTML = 'Введите имя длиннее чем 0 символов';
+    }
+    else{
+        let user = {
+            username: username,
+            isAdmin: false,
+            score: 0
+        }
+        users.push(user);
+        startGame(username);
+    }
+}
+
+function loginWithAdmin() {
+    const password = loginForm.password.value;
+
+    if (password === ''){
+        loginMessage.style.opacity = '1';
+        document.querySelector('#password').style.opacity = '1';
+    }
+
+    else {
+        if (password === 'admin'){
+            startGame('admin');
+        } else{
+            loginMessage.innerHTML = 'Неверный пароль';
+        }
+    }
 }
