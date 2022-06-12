b5ce41e179b5e7e312a9abf6ecba3e5904a9129d
dshzhkv <dashunich.f@gmail.com>
2022-06-12 22:06:32 +0500

    fix cards generation

--- public/js/restartGame.js
+++ public/js/restartGame.js
@@ -1,6 +1,3 @@
 document.querySelector("#gameStartButton").addEventListener("click", () => {
-    const cardsCount = localStorage.getItem('cardsCount');
-    localStorage.clear();
-    localStorage.setItem('cardsCount', cardsCount);
     window.location.href = '/game';
 });
