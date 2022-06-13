302fc730c3ef3e0111d711ab444bdce810198fc0
dshzhkv <dashunich.f@gmail.com>
2022-06-14 00:38:27 +0500

    fix bug with game restart in custom mode

--- public/js/restartGame.js
+++ public/js/restartGame.js
@@ -1,15 +1,9 @@
 document.querySelector("#gameStartButton").addEventListener("click", () => {
-    const cardsCount = localStorage.getItem('cardsCount');
-    updateCardsCount(cardsCount);
     window.location.href = '/game';
 });
 
 document.querySelector("#toChooseModeButton").addEventListener("click", () => {
-    updateCardsCount(12);
+    localStorage.clear();
+    localStorage.setItem('cardsCount', 12);
     window.location.href = '/mode';
 })
-
-function updateCardsCount(cardsCount){
-    localStorage.clear();
-    localStorage.setItem('cardsCount', cardsCount);
-}
